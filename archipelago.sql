522ea1edbf9051ffff69fe585e41fed320991e3c
Stephen Mather <stephen@smathermather.com>
2016-08-10 19:55:10 -0400

    whoops, re-create

---
+++ archipelago.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION where_in_the_null (crs integer) RETURNS
+geometry AS $$
+ 
+WITH null_island AS (
+    SELECT ST_MakePoint(0,0) AS geom
+    ),
+null_island_crs AS (
+    SELECT ST_SetSRID(geom, crs) AS geom FROM null_island
+)
+SELECT ST_Transform(geom, 4326) FROM null_island_crs
+  
+$$ LANGUAGE SQL VOLATILE;
+
+DROP TABLE IF EXISTS null_archipelago;
+CREATE TABLE null_archipelago AS
+SELECT srid, where_in_the_null(auth_srid) FROM spatial_ref_sys
+    WHERE srtext LIKE 'PROJCS%' AND auth_srid > 2000 AND auth_srid < 3000;
